717349e1b58b715aba09a88bdd46a63b2a912de2
Joaco1916 <31074929+Joaco1916@users.noreply.github.com>
2023-01-28 19:34:58 -0300

    Swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Todas putas menos conmigo. Atte: Tommy';
+    return 'Todes yabers menos conmigo. Atte: Didy';
   }
 }
